4b7eb7afc48bd779b0569e3fa410f3226dd18860
MaxxKolah <140779767+MaxxKolah@users.noreply.github.com>
2023-10-04 01:03:39 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,65 @@
+
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        for (int item : data) {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+
+    }
+        }
+        int[] data = new int[10];
+public class RandomDataGenerator {if __name__ == "__main__":
+
+    main()
+
+def generate_random_data():
+
+    for item in data:
+def main():
+        print(f"Random Number: {item}")
+    return data
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()import random
+    main()
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+def generate_random_data():
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+def main():
+
+
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+def generate_random_data():
+def main():
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+    data = generate_random_data()
+    main()
+
+    for item in data:
+    return data
+
+
+
+    return data
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+import random
+def generate_random_data():
+        print(f"Random Number: {item}")
+    main()
+def main():
+    data = generate_random_data()
