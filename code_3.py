f7c5a47f8d9383f92ec60f805f1e71803ae94835
MaxxKolah <140779767+MaxxKolah@users.noreply.github.com>
2023-10-05 02:17:33 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,26 @@
+
+
+def main():
+        print(f"Random Number: {item}")
+import random
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
+def generate_random_data():
+
+if __name__ == "__main__":
+    for item in data:
+    main()import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    data = generate_random_data()
+    return data
+    main()
+
+    for item in data:
+if __name__ == "__main__":
+def main():
+def generate_random_data():
+        print(f"Random Number: {item}")
