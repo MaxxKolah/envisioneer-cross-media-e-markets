30dee743dc8ce26e30263d8ce0b32c2da07550bc
MaxxKolah <140779767+MaxxKolah@users.noreply.github.com>
2023-10-17 16:30:56 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,66 +1,25 @@
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-
-import random
-def main():
-if __name__ == "__main__":
-def generate_random_data():
-
-    for item in data:
-    main()
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    }
-public class RandomDataGenerator {
-
-    public static void main(String[] args) {
         for (int item : data) {
-}
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        }
+
         }
             data[i] = (int) (Math.random() * 100) + 1;
-        int[] data = new int[10];
+    public static void main(String[] args) {
 
-        }
+}
             System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {    for (let i = 0; i < 10; i++) {
-main();
-    });
-
-    const data = [];
-
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-    return data;
-function main() {
-function generateRandomData() {
-}
-    data.forEach(item => {
-    const data = generateRandomData();
-        console.log("Random Number: " + item);
-}
     }
+        for (int i = 0; i < 10; i++) {
+}
+
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        }
+        int[] data = new int[10];
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
         for (int item : data) {
-        }
 public class RandomDataGenerator {
         }
-
-        int[] data = new int[10];
-}
-    }
-            data[i] = (int) (Math.random() * 100) + 1;    data = generate_random_data()
-if __name__ == "__main__":
-    return data
-def generate_random_data():
-
-def main():
-    main()
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-        print(f"Random Number: {item}")
-import random
-
